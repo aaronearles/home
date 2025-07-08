f9d2d6f5be93616b17c9d3ef4882cf21151df6bf
aaron.earles <aaronearles@gmail.com>
2025-07-08 14:07:48 -0600

    fix csv parse

--- scripts/linode/cli/proxy.ps1
+++ scripts/linode/cli/proxy.ps1
@@ -51,7 +51,7 @@ Write-Host "Raw output: $output" -ForegroundColor Yellow
 
 # Parse the output safely
 try {
-    $splitOutput = $output | ConvertFrom-Csv -Header id, label, region, type, image, status, ipv4
+    $splitOutput = $output | ConvertFrom-Csv -Header id, label, region, type, image, status, ipv4, disk_encryption, interface_generation, placement_group_label
     $linodeCliOutput = $splitOutput | Select-Object -Skip 2
     $newhostip = $linodeCliOutput.ipv4
     $newhostid = $linodeCliOutput.id
